214b469205f5caa7e80ab63cbaa878725673c512
Jay Liu <jliu2589@gmail.com>
2023-09-26 15:56:56 -0400

    connect frontend to sanity

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,38 @@
+// ./src/app/page.tsx
+// ./src/utils/sanity/client.ts
+import { createClient } from 'next-sanity';
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID; // "pv8y60vp"
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET; // "production"
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05-03';
+
+const client = createClient({
+  projectId,
+  dataset,
+  apiVersion, // https://www.sanity.io/docs/api-versioning
+  useCdn: true, // if you're using ISR or only static generation at build time then you can set this to `false` to guarantee no stale content
+});
+
+type Post = {
+  _id: string;
+  title?: string;
+  slug?: {
+    current: string;
+  };
+};
+
+export default async function PostIndex() {
+  const posts = await client.fetch<Post[]>(`*[_type == "post"]`);
+
+  console.log(posts);
+
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post._id}>
+          <a href={post?.slug.current}>{post?.title}</a>
+        </li>
+      ))}
+    </ul>
+  );
+}
