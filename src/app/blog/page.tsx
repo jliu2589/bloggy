05120516e4fbdc9d13cf0f998339d5c34234b199
Jay Liu <jliu2589@gmail.com>
2023-09-27 10:09:54 -0400

    RichTextCompoents, PortableTextEditor, Body now displayed

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,12 +1,14 @@
 // ./src/app/page.tsx
 // ./src/utils/sanity/client.ts
 import { createClient } from 'next-sanity';
+import { PortableText } from '@portabletext/react';
+import { RichTextComponents } from '../../../RichTextComponents';
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID; // "pv8y60vp"
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET; // "production"
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05-03';
 
-const client = createClient({
+export const client = createClient({
   projectId,
   dataset,
   apiVersion, // https://www.sanity.io/docs/api-versioning
@@ -19,6 +21,7 @@ type Post = {
   slug?: {
     current: string;
   };
+  body: any;
 };
 
 export default async function PostIndex() {
@@ -31,6 +34,7 @@ export default async function PostIndex() {
       {posts.map((post) => (
         <li key={post._id}>
           <a href={post?.slug.current}>{post?.title}</a>
+          <PortableText value={post.body} components={RichTextComponents} />
         </li>
       ))}
     </ul>
